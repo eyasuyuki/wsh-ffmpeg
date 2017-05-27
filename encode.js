5b92e6264320bd4885cc574e1bd42155f541c6c1
ENDO Yasuyuki <eyasuyuki@gmail.com>
2017-05-27 19:48:58 +0900

    fix ffmpegpath

--- encode.js
+++ encode.js
@@ -3,7 +3,7 @@
  */
 (function () {
     // set ffmpeg path
-    var ffmpegpath = '%comspec% /c "%AppData%\\ffmpeg-20170520-64ea4d1-win64-static\\bin\\ffmpeg.exe"';
+    var ffmpegpath = '%AppData%\\ffmpeg-20170520-64ea4d1-win64-static\\bin\\'
 
     // get arguments
     var filePath = WScript.Arguments(0);
@@ -13,10 +13,11 @@
     var outPath = p.join('\\');
     var outFile = f[0]+'.mp4';
 
-    var exec = ffmpegpath+' -fflags +discardcorrupt -i '+filePath+' -fpre %AppData%\\ffmpeg-20170520-64ea4d1-win64-static\\bin\\x264_main.ffpreset -aspect 16:9 -pass 1  -passlogfile C:\\Users\\yasuyuki\\Videos\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec aac -profile:a aac_low -strict experimental '+outPath+'\\'+outFile+' & pause';
+    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec aac -profile:a aac_low -strict experimental ' + outPath + '\\' + outFile + ' & pause';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
+
     //shell.Run(exec);
     shell.run(exec);
 
