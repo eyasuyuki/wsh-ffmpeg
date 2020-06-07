8c80804b2131614c24a53250388b8eb1f5d9e2f3
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-08 08:32:50 +0900

    revert

--- encode.js
+++ encode.js
@@ -12,9 +12,8 @@
     var f = fileName.split('.');
     var outPath = p.join('\\');
     var outFile = f[0]+'.mp4';
-    var out = '"'+outPath+'\\'+outFile+'"';
 
-    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
+    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + outPath + '\\' + outFile + ' & pause';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
