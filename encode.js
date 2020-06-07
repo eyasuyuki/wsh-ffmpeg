112e715c2ece8e2127c79ec4dbbb4481f5f0a48d
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-08 07:32:52 +0900

    line terminator CR+LF

--- encode.js
+++ encode.js
@@ -10,10 +10,11 @@
     var p = filePath.split('\\');
     var fileName = p.pop();
     var f = fileName.split('.');
-    var outPath = p.join('\\');
-    var outFile = f[0]+'.mp4';
 
-    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + outPath + '\\' + outFile + ' & pause';
+    var outFile = f[0]+'.mp4';
+    var out = '"'+outPath+'\\'+outFile+'"';
+
+    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
