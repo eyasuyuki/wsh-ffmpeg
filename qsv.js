386244acc1cdc7b129f9306ce66a5c7203b92924
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-08 08:19:17 +0900

    add double-quote to input file path

--- qsv.js
+++ qsv.js
@@ -7,6 +7,7 @@
 
     // get arguments
     var filePath = WScript.Arguments(0);
+    var file = '"'+filePath+'"';
     var p = filePath.split('\\');
     var fileName = p.pop();
     var f = fileName.split('.');
@@ -14,7 +15,7 @@
     var outFile = f[0]+'.mp4';
     var out = '"'+outPath+'\\'+outFile+'"';
 
-    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -init_hw_device qsv:hw -i ' + filePath + ' -loglevel info -pix_fmt nv12 -c:v h264_qsv -async_depth 4 -avbr_accuracy 0 -avbr_convergence 0 -preset veryslow -vcm 0 -bitrate_limit 1 -cavlc 0 -idr_interval 0 -pic_timing_sei 1 -max_dec_frame_buffering 0 -look_ahead 0 -look_ahead_depth 0 -look_ahead_downsampling off -trellis:v P -profile:v high -b:v 3834000 -g 74 -bf 2 -slices 1 -refs 3 -b_strategy 1 -flags +cgop -a53cc 0 -maxrate 3834000 -bufsize 4474111  -compression_level 10 -c:a ac3  -ac 6 -y ' + out + ' & pause';
+    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -init_hw_device qsv:hw -i ' + file + ' -loglevel info -pix_fmt nv12 -c:v h264_qsv -async_depth 4 -avbr_accuracy 0 -avbr_convergence 0 -preset veryslow -vcm 0 -bitrate_limit 1 -cavlc 0 -idr_interval 0 -pic_timing_sei 1 -max_dec_frame_buffering 0 -look_ahead 0 -look_ahead_depth 0 -look_ahead_downsampling off -trellis:v P -profile:v high -b:v 3834000 -g 74 -bf 2 -slices 1 -refs 3 -b_strategy 1 -flags +cgop -a53cc 0 -maxrate 3834000 -bufsize 4474111  -compression_level 10 -c:a ac3  -ac 6 -y ' + out + ' & pause';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
