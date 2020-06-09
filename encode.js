f1476c8a46754f198a087aedf5802389ea922bf6
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-09 13:25:33 +0900

    debug Echo

--- encode.js
+++ encode.js
@@ -17,6 +17,8 @@
 
     var exec = '%comspec% /c ' + ffmpegpath +'ffmpeg.exe -fflags +discardcorrupt -i ' + inFile + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
 
+    WshShell.Echo(exec);
+
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
 
