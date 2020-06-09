fd4eeeaffee9f9128bc01c182ba68416c1fc6e7a
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-09 13:11:10 +0900

    use \u0022 to filename quote

--- encode.js
+++ encode.js
@@ -7,13 +7,15 @@
 
     // get arguments
     var filePath = WScript.Arguments(0);
+    var inFile = "" + "\u0022" + filePath + "\u0022" + "";
     var p = filePath.split('\\');
     var fileName = p.pop();
     var f = fileName.split('.');
     var outPath = p.join('\\');
     var outFile = f[0]+'.mp4';
+    var out = "" + "\u0022" + outPath + '\\' + outFile + "\u0022" + "";
 
-    var exec = '%comspec% /c "' + ffmpegpath +'ffmpeg.exe" -fflags +discardcorrupt -i ' + filePath + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + outPath + '\\' + outFile + ' & pause';
+    var exec = '%comspec% /c ' + ffmpegpath +'ffmpeg.exe -fflags +discardcorrupt -i ' + inFile + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
