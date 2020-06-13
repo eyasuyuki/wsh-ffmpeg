ac17a62fccd75c38cc112146e8d6c3b58c88d299
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-14 07:47:52 +0900

    use Enumerator

--- encode.js
+++ encode.js
@@ -3,15 +3,17 @@
  */
 (function () {
     // set ffmpeg path
-    var ffmpegpath = '%AppData%\\ffmpeg-20180201-b1af0e2-win64-static\\bin\\'
-
-    // get arguments
-    For Each filePath in WScript.Arguments
-	enc(filePath)
-    Next
-)();
+    var e = new Enumerator(WScript.Arguments);
+    e.moveFirst();
+    while (e.atEnd() == false) {
+	var filePath = e.item();
+	enc(filePath);
+	e.moveNext();
+    }
+})();
 
 function enc(filePath) {
+    var ffmpegpath = '%AppData%\\ffmpeg-20180201-b1af0e2-win64-static\\bin\\'
     var inFile = "" + "\u0022" + filePath + "\u0022" + "";
     var p = filePath.split('\\');
     var fileName = p.pop();
@@ -21,8 +23,6 @@ function enc(filePath) {
     var out = "" + "\u0022" + outPath + '\\' + outFile + "\u0022" + "";
 
     var exec = '%comspec% /c ' + ffmpegpath +'ffmpeg.exe -fflags +discardcorrupt -i ' + inFile + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
-
-    WshShell.Echo(exec);
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
