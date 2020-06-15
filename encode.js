fb6376eb595572648bdeba535f99ea18ccbd67a7
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-06-15 10:38:32 +0900

    remove pause

--- encode.js
+++ encode.js
@@ -22,7 +22,7 @@ function enc(filePath) {
     var outFile = f[0]+'.mp4';
     var out = "" + "\u0022" + outPath + '\\' + outFile + "\u0022" + "";
 
-    var exec = '%comspec% /c ' + ffmpegpath +'ffmpeg.exe -fflags +discardcorrupt -i ' + inFile + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' & pause';
+    var exec = '%comspec% /c ' + ffmpegpath +'ffmpeg.exe -fflags +discardcorrupt -i ' + inFile + ' -fpre ' + ffmpegpath + 'x264.ffpreset -aspect 16:9 -pass 1  -passlogfile %temp%\\pass.log -filter:v yadif -flags +loop+global_header -vcodec libx264 -acodec ac3 -ac 6 -profile:a aac_low -strict experimental ' + out + ' ';
 
     // execute command
     var shell = WScript.CreateObject( "WScript.Shell" );
